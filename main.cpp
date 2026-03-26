f251119b0a3a5eb536238797f3266027f25ac7d4
Floreos <saxygeorgesmith@gmail.com>
2026-03-26 10:22:36 -0500

    Made a hello world program
    It puts hello world in the console

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//George Smith Coding 1 Spring 2026
+//Our first c++ program
+//compile in terminal with "g++ main.cpp -o main"
+//use "cp 'FolderName'"" to navigate to the path of the c++ file
+
+#include <iostream>     //in-out stream for sending text to the console
+using namespace std;    
+//So we dont have to type "std::"" all the time
+//this is where our program starts running code
+int main() 
+{
+    cout << "Hello World!" << endl;     //cout for console out
+                                        //endl for end line
+    return 0;
+}
