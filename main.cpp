aba8a59a2bcc3e8484fd8b2bcbecca66cdce688e
Floreos <saxygeorgesmith@gmail.com>
2026-03-26 10:24:15 -0500

    Updated instructions
    to include a run command

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //George Smith Coding 1 Spring 2026
 //Our first c++ program
 //compile in terminal with "g++ main.cpp -o main"
+//then run with "./main"
 //use "cp 'FolderName'"" to navigate to the path of the c++ file
 
 #include <iostream>     //in-out stream for sending text to the console
