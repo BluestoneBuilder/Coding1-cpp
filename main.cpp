ec3c759c648f767f35ad2ff0ad126dd2386c84d5
Floreos <saxygeorgesmith@gmail.com>
2026-03-26 10:31:28 -0500

    Added player input
    Asks player their name. They enter it and it says hello to them

--- main.cpp
+++ main.cpp
@@ -5,12 +5,25 @@
 //use "cp 'FolderName'"" to navigate to the path of the c++ file
 
 #include <iostream>     //in-out stream for sending text to the console
+#include <string>       //for player name
+
 using namespace std;    
+
 //So we dont have to type "std::"" all the time
 //this is where our program starts running code
 int main() 
 {
-    cout << "Hello World!" << endl;     //cout for console out
+    string playerName;                  //variable name written in camelCase
+
+    //ask the player their name
+    cout << "What is your name, player? ";
+
+    //get input from player with cin
+    cin >> playerName;
+
+    cout << "Hello " << playerName << "." << endl;
+
+    //cout << "Hello World!" << endl;     //cout for console out
                                         //endl for end line
     return 0;
 }
