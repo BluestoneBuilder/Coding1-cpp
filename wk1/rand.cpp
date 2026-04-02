89a5d87fc6db10ba355d72f74a09bb08c873292b
Floreos <saxygeorgesmith@gmail.com>
2026-04-02 10:10:42 -0500

    Made random numbers script

---
+++ wk1/rand.cpp
@@ -0,0 +1,20 @@
+//George Smith, Coding 1, Sring 2026
+//Switch, random numbers, and while loops
+
+//You task... create the rest of the code to say "Random Numbers!"
+//Then run the program.
+
+//Open terminal
+//use "cp path/to/folder" to navigate to the path of the c++ file
+//"cd GitHub/Coding1-cpp/wk1" for this script
+//compile with "g++ rand.cpp -o rand"
+//then run with "./rand"
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Random Numbers!\n\n";
+    return 0;
+}
