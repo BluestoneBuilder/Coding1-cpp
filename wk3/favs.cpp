6a560c0225c8615cdcad722624acf284274068f3
Floreos <saxygeorgesmith@gmail.com>
2026-04-14 14:31:01 -0500

    Added line spaces and add indicator

--- wk3/favs.cpp
+++ wk3/favs.cpp
@@ -14,7 +14,7 @@ int main()
 {
     vector<string> favs;
     while(true){
-        cout << "What would you like to do?\n";
+        cout << "\nWhat would you like to do?\n";
         cout << "'add', 'edit', 'remove', 'show', or 'quit'.\n";
 
         string input;
@@ -35,25 +35,26 @@ int main()
         }
         else if(input == "quit")
         {
-            cout << "Thanks for playing!\n";
+            cout << "\nThanks for playing!\n";
             break;
         }
         else if(input == "add")
         {
-            cout << "What game would you like to add to your favorites?\n";
+            cout << "\nWhat game would you like to add to your favorites?\n";
             getline(cin, input);
             favs.push_back(input);
+            cout << "\nAdded '" << input << "'\n";
         }
         else if(input == "show")
         {
             sort(favs.begin(), favs.end());
             if(favs.size() == 0)
             {
-                cout << "You don't have any favorites right now. Consider using the 'add' function\n";
+                cout << "\nYou don't have any favorites right now. Consider using the 'add' function\n";
             }
             else
             {
-                cout << "Here are your current favorite games.\n";
+                cout << "\nHere are your current favorite games.\n";
                 for(int i = 0; i < favs.size(); i++)
                 {
                     cout << i + 1 << ". " << favs[i] << endl;
@@ -64,24 +65,24 @@ int main()
         {
             if(favs.size() == 0)
             {
-                cout << "You don't have any favorites right now. Consider using the 'add' function\n";
+                cout << "\nYou don't have any favorites right now. Consider using the 'add' function\n";
             }
             else
             {
-                cout << "Which of your favorites would you like to edit?\n";
+                cout << "\nWhich of your favorites would you like to edit?\n";
                 getline(cin, input);
                 vector<string>::iterator favToEdit = favs.begin();
                 favToEdit = find(favs.begin(), favs.end(), input);
                 if(favToEdit != favs.end())
                 {
-                    cout << "What would you like to change '" << *favToEdit << "' to?\n";\
+                    cout << "\nWhat would you like to change '" << *favToEdit << "' to?\n";\
                     getline(cin, input);
-                    cout << "'" << *favToEdit << "' has been changed to '" << input << "'.\n";
+                    cout << "\n'" << *favToEdit << "' has been changed to '" << input << "'.\n";
                     *favToEdit = input;
                 }
                 else
                 {
-                    cout << "I couldn't find that game in your favorites\n";
+                    cout << "\nI couldn't find that game in your favorites\n";
                 }
             }
         }
@@ -89,28 +90,28 @@ int main()
         {
             if(favs.size() == 0)
             {
-                cout << "You don't have any favorites right now. Consider using the 'add' function\n";
+                cout << "\nYou don't have any favorites right now. Consider using the 'add' function\n";
             }
             else
             {
-                cout << "What game would you like to remove from your favorites?\n";
+                cout << "\nWhat game would you like to remove from your favorites?\n";
                 getline(cin, input);
                 vector<string>::iterator favToRemove = favs.begin();
                 favToRemove = find(favs.begin(), favs.end(), input);
                 if(favToRemove != favs.end())
                 {
-                    cout << "Removed '" << *favToRemove << "'.\n";
+                    cout << "\nRemoved '" << *favToRemove << "'.\n";
                     favs.erase(favToRemove);
                 }
                 else
                 {
-                    cout << "I couldn't find that game in your favorites\n";
+                    cout << "\nI couldn't find that game in your favorites\n";
                 }
             }
         }
         else
         {
-            cout << "Unrecognized input.\n";
+            cout << "\nUnrecognized input.\n";
         }
     }
 }
