ec7a069252bc53696e7b86a2c040a1d420ff8bf5
Floreos <saxygeorgesmith@gmail.com>
2026-04-14 10:48:19 -0500

    Finished vec and started favs assignment

--- wk2/vec.cpp
+++ wk2/vec.cpp
@@ -1,7 +1,9 @@
 //George Smith
 //Vectors, iterators, and algorithms
 //g++ vec.cpp -o vec && ./vec
+//ctrl+C to quit loop
 #include <iostream>
+#include <ostream>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -11,88 +13,119 @@ int main()
 {
     cout << "Let's learn about vectors!\n";
 
-    int chapter = 0;
-    cout << "What program would you like to run?\n";
-    cout << "Press 1 for Vectors.\n";
-    cout << "Press 2 for push_back() and pop_back().\n";
-    cout << "Press 3 for algorithms.\n";
+    //for chapter 4
+    vector<string> favs = {"Sifu", "HollowKnight", "Zelda", "Sekiro", "EldenRing"};
 
-    cin >> chapter;
-    if(chapter == 1)
+    do
     {
-        cout << "You have selected Vectors.\n\n";
-        //collection initializer
-        vector<string> names = {"George", "Brian", "otherDude", "pipo"};
-        cout << "There are " << names.size() << " names in the vector.\n";
-        cout << "Here are your names:\n";
-        for(int i = 0; i < names.size(); i++)
-        {
-            cout << i + 1 << ". " << names[i] << endl;
-        }                                               //iterator that points to specific spot in vector
-        cout << "The first name in the vector is " << *(names.begin()) << ".\n";
+        int chapter = 0;
+        cout << "What program would you like to run?\n";
+        cout << "Press 1 for Vectors.\n";
+        cout << "Press 2 for push_back() and pop_back().\n";
+        cout << "Press 3 for algorithms.\n";
+        cout << "Press 4 for removing.\n";
 
-        //Iterator. Points to an element in a vector;
-        vector<string>::iterator iter;
-        iter = names.begin() + 2;
-
-        //dereference iter with *
-        //
-        cout << "Iter is pointing at " << *iter << ".\n";
-    }
-    else if(chapter == 2)
-    {
-        cout << "Let's use push_back() and pop_back().\n";
-        vector<string> favMovies;
-        while(favMovies.size() < 3)
+        cin >> chapter;
+        if(chapter == 1)
         {
-            cout << "Please add a favorite movie: ";
+            cout << "You have selected Vectors.\n\n";
+            //collection initializer
+            vector<string> names = {"George", "Brian", "otherDude", "pipo"};
+            cout << "There are " << names.size() << " names in the vector.\n";
+            cout << "Here are your names:\n";
+            for(int i = 0; i < names.size(); i++)
+            {
+                cout << i + 1 << ". " << names[i] << endl;
+            }                                               //iterator that points to specific spot in vector
+            cout << "The first name in the vector is " << *(names.begin()) << ".\n";
+
+            //Iterator. Points to an element in a vector;
+            vector<string>::iterator iter;
+            iter = names.begin() + 2;
+
+            //dereference iter with *
+            //
+            cout << "Iter is pointing at " << *iter << ".\n";
+        }
+        else if(chapter == 2)
+        {
+            cout << "Let's use push_back() and pop_back().\n";
+            vector<string> favMovies;
+            while(favMovies.size() < 3)
+            {
+                cout << "Please add a favorite movie: ";
+                string input;
+                cin >> input;
+
+                favMovies.push_back(input);
+            }
+            cout << "Here are your favorite movies:\n";
+            for(vector<string>::iterator iter = favMovies.begin(); iter != favMovies.end(); iter++)
+            {
+                cout << *iter << endl;
+            }
+        }
+        else if(chapter == 3)
+        {
+            vector<string> friends;
+            friends.push_back("Bobbert");
+            friends.push_back("Pipo");
+            friends.push_back("Jonathaniel");
+            friends.push_back("Gableer");
+
+            cout << "Here are your friends:\n";
+            for(int i = 0; i < friends.size(); i++)
+            {
+                cout << friends[i] << ".\n";
+            }
+            cout << "Please choose a friend to visit:\n";
+            vector<string>::iterator chosenFriend = friends.begin();
             string input;
             cin >> input;
 
-            favMovies.push_back(input);
+            //use an algorithm to find a friend
+            chosenFriend = find(friends.begin(), friends.end(), input);
+            if(chosenFriend != friends.end())
+            {
+                cout << "Let's talk to " << *chosenFriend << " today!\n";
+            }
+            else 
+            {
+                cout << "I couldn't find " << input << " in your friends list.\n";
+            }
+            cout << "Let's give " << *chosenFriend << " a nickname!\n";
+            cin >> input;
+            *chosenFriend = input;
+            cout << "Their nickname is now " << *chosenFriend << ".\n";
         }
-        cout << "Here are your favorite movies:\n";
-        for(vector<string>::iterator iter = favMovies.begin(); iter != favMovies.end(); iter++)
+        else if(chapter == 4)
         {
-            cout << *iter << endl;
-        }
-    }
-    else if(chapter == 3)
-    {
-        vector<string> friends;
-        friends.push_back("Bobbert");
-        friends.push_back("Pipo");
-        friends.push_back("Jonathaniel");
-        friends.push_back("Gableer");
+            cout << "Let's remove an element from a global vector (global-ish).\n";
+            sort(favs.begin(), favs.end());
+            
+            for(int i = 0; i < favs.size(); i++)
+            {
+                cout << favs[i] << endl;
+            }
+            cout << "What name should we remove?\n";
+            string input;
+            cin >> input;
 
-        cout << "Here are your friends:\n";
-        for(int i = 0; i < friends.size(); i++)
-        {
-            cout << friends[i] << ".\n";
-        }
-        cout << "Please choose a friend to visit:\n";
-        vector<string>::iterator chosenFriend = friends.begin();
-        string input;
-        cin >> input;
-
-        //use an algorithm to find a friend
-        chosenFriend = find(friends.begin(), friends.end(), input);
-        if(chosenFriend != friends.end())
-        {
-            cout << "Let's talk to " << *chosenFriend << " today!\n";
+            //auto becomes whatever var type you plug into it
+            auto iter = find(favs.begin(), favs.end(), input);
+            if(iter != favs.end())
+            {
+                cout << "Found it!\n";
+                favs.erase(iter);
+            }
+            else 
+            {
+                cout << "I couldn't find that in your favs.\n";
+            }
         }
         else 
         {
-            cout << "I couldn't find " << input << " in your friends list.\n";
+            cout << "Chapter " << chapter << " isn't one of the options.\n";
         }
-        cout << "Let's give " << *chosenFriend << " a nickname!\n";
-        cin >> input;
-        *chosenFriend = input;
-        cout << "Their nickname is now " << *chosenFriend << ".\n";
-    }
-    else 
-    {
-        cout << "Chapter " << chapter << " isn't one of the options.\n";
-    }
-    return 0;
+    } while(true);
 }
---
+++ wk3/favs.cpp
@@ -0,0 +1,88 @@
+//George Smith
+//create a list of favorites
+//g++ favs.cpp -o favs && ./favs
+//ctrl+C to quit loop
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<string> favs;
+    while(true){
+        cout << "What would you like to do?\n";
+        cout << "'add', 'edit', 'remove', 'show', or 'quit'.\n";
+
+        string input;
+        getline(cin, input);
+
+
+        if(input == "test")
+        {
+            cout << "use getline for number with stoi(input):\n";
+            cout << "What's your favorite number?\n";
+            getline(cin, input);
+            int favNum = stoi(input);
+
+            if(favNum > 3)
+            {
+                cout << "Okay, man. Your favorite number is bigger than mine. Show off.\n";
+            }
+        }
+        else if(input == "quit")
+        {
+            cout << "Thanks for playing!\n";
+            break;
+        }
+        else if(input == "add")
+        {
+            cout << "What game would you like to add to your favorites?\n";
+            getline(cin, input);
+            favs.push_back(input);
+        }
+        else if(input == "show")
+        {
+            sort(favs.begin(), favs.end());
+            if(favs.size() == 0)
+            {
+                cout << "You don't have any favorites right now. Consider using the 'add' function\n";
+            }
+            else
+            {
+                cout << "Here are your current favorite games.\n";
+                for(int i = 0; i < favs.size(); i++)
+                {
+                    cout << i + 1 << ". " << favs[i] << endl;
+                }
+            }
+        }
+        else if(input == "edit")
+        {
+
+        }
+        else if(input == "remove")
+        {
+            cout << "What game would you like to remove from your favorites?\n";
+            getline(cin, input);
+            vector<string>::iterator favToRemove = favs.begin();
+            favToRemove = find(favs.begin(), favs.end(), input);
+            if(favToRemove != favs.end())
+            {
+                favs.erase(favToRemove);
+                cout << "Removed '" << *favToRemove << "'.\n";
+            }
+            else
+            {
+                cout << "I couldn't find that game in your favorites\n";
+            }
+        }
+        else
+        {
+            cout << "Unrecognized input.\n";
+        }
+    }
+}
